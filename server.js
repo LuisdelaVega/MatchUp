14c722e31b98e5ea1ddf1deea29c9e55556bfc2c
luisdelavega <luisr.delavega@gmail.com>
2015-03-09 21:25:12 -0400

    Lets try this then

--- server.js
+++ server.js
@@ -18,12 +18,19 @@ var secret = '7h1s h6Re i5 th6 p6rf6c7 plac6 t0 m4kE 4 Nyx A5s4s51n j0k6!';
 
 var app = express();
 
-app.use(function(req, res, next) {
+var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-	next();
-});
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+	// intercept OPTIONS method
+	if ('OPTIONS' == req.method) {
+		res.send(200);
+	} else {
+		next();
+	}
+};
+
+app.use(allowCrossDomain);
 
 // We are going to protect /api routes with JWT
 app.use('/matchup', expressJwt({
