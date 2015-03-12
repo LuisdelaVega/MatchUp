0ff931e842df9a497eafaac83876842fa61febbf
luisdelavega <luisr.delavega@gmail.com>
2015-03-12 17:38:27 -0400

    Finally the bracket generation algorithm is 100% done. Separated the algorithm into single and double elimination brackets

--- server.js
+++ server.js
@@ -37,29 +37,33 @@ app.use(express.static(__dirname + '/public'));
 
 /////////////////////////////////////////////////////////////////////////////////////////// HANDLERS
 
-function createTournament(req, res) {
-	var tournament = new Object();
-	var tournamentFilter = new Array();
-
-	tournament.numOfPlayers = req.params.numofplayers;
-	tournament.byes = getByes(tournament.numOfPlayers);
-	tournament.winnerRounds = new Array();
-	//array of round objects
-	tournament.loserRounds = new Array();
-	//array of round objects
-	tournament.numOfWinnerRounds = 0;
-	tournament.numOfLoserRounds = 0;
+function createBraket(req, res) {
+	var bracket = new Object();
+	bracket.numOfPlayers = req.params.numofplayers;
+	bracket.byes = getByes(bracket.numOfPlayers);
+	bracket.numOfWinnerRounds = 0;
 
 	var players = new Array();
 	var i = 0;
-	for ( i = 0; i < tournament.numOfPlayers; i++) {
+	for ( i = 0; i < bracket.numOfPlayers; i++) {
 		players[i] = new Object();
 		players[i].seed = i + 1;
 	}
 
-	generateBracket(tournament, players);
+	if (req.params.type == 'single') {
+		bracket.winnerRounds = new Array();
+		singleEliminationBracket(bracket, players);
+	} else if (req.params.type == 'double') {
+		bracket.winnerRounds = new Array();
+		singleEliminationBracket(bracket, players);
+		bracket.numOfLoserRounds = 0;
+		bracket.loserRounds = new Array();
+		doubleEliminationBracket(bracket);
+	} else {
+		return res.status(401);
+	}
 
-	res.send(tournament);
+	res.send(bracket);
 }
 
 function getMyProfile(req, res) {
@@ -88,7 +92,7 @@ function authenticate(req, res) {
 		if (err) {
 			return console.error('error fetching client from pool', err);
 		}
-		
+
 		// Query the database to find the account
 		var query = client.query({
 			text : "SELECT * FROM customer WHERE customer_username = $1 AND customer_password = $2",
@@ -117,12 +121,12 @@ function authenticate(req, res) {
 	});
 };
 
-function getUserProfile(req, res){
+function getUserProfile(req, res) {
 	res.send("Hello!");
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////// ROUTES
-app.get('/test/:numofplayers', createTournament);
+app.get('/test/:type/:numofplayers', createBraket);
 app.post('/login', authenticate);
 app.get('/matchup/profile', getMyProfile);
 app.get('/matchup/profile/:username', getUserProfile);
@@ -156,98 +160,44 @@ function getLosersRound1Matches(numOfPlayers) {
 	return getWinnersRoundMatches(numOfPlayers) % (getNumOfPlayersForNextWinnersRound(numOfPlayers) / 2);
 }
 
-function generateBracket(tournament, players) {
+function singleEliminationBracket(bracket, players) {
 	// calculate winner rounds matches
 	var winnerRounds = new Array();
 	var i = 0;
-	var winners = tournament.numOfPlayers;
+	var winners = bracket.numOfPlayers;
 	for ( i = 0; winners > 1; i++) {
 		winnerRounds[i] = new Object();
 		winnerRounds[i].name = "Winners Round " + (i + 1);
 		//console.log(winnerRounds[i].name);
 		winnerRounds[i].amountOfMatches = getWinnersRoundMatches(winners);
 		//console.log("Amount of matches: " + winnerRounds[i].amountOfMatches);
-		tournament.numOfWinnerRounds++;
+		bracket.numOfWinnerRounds++;
 
 		winners = getNumOfPlayersForNextWinnersRound(winners);
 	}
-	winnerRounds[i] = new Object();
-	winnerRounds[i].name = "Winners Round " + (i + 1);
-	//console.log(winnerRounds[i].name);
-	winnerRounds[i].amountOfMatches = 1;
-	//console.log("Amount of matches: " + winnerRounds[i].amountOfMatches);
-	tournament.numOfWinnerRounds++;
 
-	//console.log(tournament.numOfPlayers);
+	//console.log(bracket.numOfPlayers);
 
-	var loserRounds = new Array();
-	loserRounds[0] = new Object();
-	loserRounds[0].name = "Losers Round 1";
-	//console.log(loserRounds[0].name);
-	loserRounds[0].amountOfMatches = getLosersRound1Matches(tournament.numOfPlayers);
-	//console.log("Amount of matches: "+loserRounds[0].amountOfMatches);
-	tournament.numOfLoserRounds++;
-
-	// Calculate loser rounds matches
-	i = 0;
-	if (winnerRounds[0].amountOfMatches > winnerRounds[1].amountOfMatches) {
-		loserRounds[1] = new Object();
-		loserRounds[1].name = "Losers Round 2";
-		//console.log(loserRounds[1].name);
-		loserRounds[1].amountOfMatches = winnerRounds[1].amountOfMatches;
-		//console.log("Amount of matches: "+loserRounds[1].amountOfMatches);
-		tournament.numOfLoserRounds++;
-		i = 1;
-	}
-	var j = 1;
-	var count = 0;
-	do {
-		i++;
-		loserRounds[i] = new Object();
-		loserRounds[i].name = "Losers Round " + (i + 1);
-		//console.log(loserRounds[i].name);
-		loserRounds[i].amountOfMatches = (winnerRounds[j].amountOfMatches) / 2;
-		//console.log("Amount of matches: "+loserRounds[i].amountOfMatches);
-		tournament.numOfLoserRounds++;
-		count++;
-		if (!(count % 2)) {
-			j++;
-		}
-	} while(loserRounds[i].amountOfMatches > 1);
-	loserRounds[i + 1] = new Object();
-	loserRounds[i + 1].name = "Losers Round " + (i + 1 + 1);
-	//console.log(loserRounds[i + 1].name);
-	loserRounds[i + 1].amountOfMatches = 1;
-	//console.log("Amount of matches: "+loserRounds[i+1].amountOfMatches);
-	tournament.numOfLoserRounds++;
-
-	//Create matches of rounds
-	for ( i = 0; i < tournament.numOfWinnerRounds; i++) {
+	//Create the Matches for every Round
+	for ( i = 0; i < bracket.numOfWinnerRounds; i++) {
 		winnerRounds[i].matches = new Array();
 		for ( j = 0; j < winnerRounds[i].amountOfMatches; j++) {
 			winnerRounds[i].matches[j] = new Object();
 			winnerRounds[i].matches[j].name = "Winners Round " + (i + 1) + " Match " + (j + 1);
 		}
 	}
-	for ( i = 0; i < tournament.numOfLoserRounds; i++) {
-		loserRounds[i].matches = new Array();
-		for ( j = 0; j < loserRounds[i].amountOfMatches; j++) {
-			loserRounds[i].matches[j] = new Object();
-			loserRounds[i].matches[j].name = "Losers Round " + (i + 1) + " Match " + (j + 1);
-		}
-	}
 
-	// Assign players for winners Round 1 matches
-	for ( i = 0; i < (tournament.numOfPlayers - tournament.byes) / 2; i++) {
-		winnerRounds[0].matches[i].player1 = players[tournament.byes + i/* - 1*/];
-		winnerRounds[0].matches[i].player2 = players[tournament.numOfPlayers - i - 1];
+	// Assign players for Winners Round 1 Matches
+	for ( i = 0; i < (bracket.numOfPlayers - bracket.byes) / 2; i++) {
+		winnerRounds[0].matches[i].player1 = players[bracket.byes + i/* - 1*/];
+		winnerRounds[0].matches[i].player2 = players[bracket.numOfPlayers - i - 1];
 		//console.log(winnerRounds[0].name+", match: "+i+", player1: "+winnerRounds[0].matches[i].player1.seed+", player2: "+winnerRounds[0].matches[i].player2.seed);
 	}
-	i = 0;
-	if (tournament.byes) {
-		// Assign players to winners Round 2 matches
+
+	if (bracket.byes) {
+		// Assign players to Winners Round 2 matches
 		var count = 0;
-		for ( i = 0; i < tournament.byes; i++) {
+		for ( i = 0; i < bracket.byes; i++) {
 			if (i < winnerRounds[1].amountOfMatches) {
 				winnerRounds[1].matches[i].player1 = players[i];
 				//console.log(winnerRounds[1].name+", match: "+i+", player1: "+winnerRounds[1].matches[i].player1.seed);
@@ -263,125 +213,199 @@ function generateBracket(tournament, players) {
 		// Winners Round 1 - Winner goes to
 		for ( i = 0; i < winnerRounds[0].amountOfMatches; i++) {
 			if (i < (winnerRounds[0].amountOfMatches - winnerRounds[1].amountOfMatches)) {
-				winnerRounds[0].matches[i].winnerGoesTo = winnerRounds[1].matches[winnerRounds[0].amountOfMatches + i - ((winnerRounds[0].amountOfMatches - winnerRounds[1].amountOfMatches) * 2)];
+				winnerRounds[0].matches[i].winnerGoesTo = winnerRounds[1].matches[winnerRounds[0].amountOfMatches + i - ((winnerRounds[0].amountOfMatches - winnerRounds[1].amountOfMatches) * 2)].name;
 			} else {
-				winnerRounds[0].matches[i].winnerGoesTo = winnerRounds[1].matches[winnerRounds[0].amountOfMatches - i - 1];
-			}
-		}
-		// Losers Round 1 - Winner goes to
-		for ( i = 0; i < loserRounds[0].amountOfMatches; i++) {
-			if (i < (loserRounds[0].amountOfMatches - loserRounds[1].amountOfMatches)) {
-				loserRounds[0].matches[i].winnerGoesTo = loserRounds[1].matches[loserRounds[0].amountOfMatches + i - ((loserRounds[0].amountOfMatches - loserRounds[1].amountOfMatches) * 2)];
-			} else {
-				loserRounds[0].matches[i].winnerGoesTo = loserRounds[1].matches[loserRounds[0].amountOfMatches - i - 1];
-			}
-		}
-		// Winners Round 1 & 2 - Loser goes to
-		var temp = i;
-		if (winnerRounds[0].amountOfMatches > loserRounds[0].amountOfMatches) {
-			for ( i = 0; i < winnerRounds[0].amountOfMatches; i++) {
-				if (i < (winnerRounds[0].amountOfMatches - winnerRounds[1].amountOfMatches)) {
-					winnerRounds[0].matches[i].loserGoesTo = loserRounds[0].matches[i];
-				} else if (i < ((winnerRounds[0].amountOfMatches - winnerRounds[1].amountOfMatches) * 2)) {
-					winnerRounds[0].matches[i].loserGoesTo = loserRounds[0].matches[loserRounds[0].amountOfMatches - i + 1];
-				} else {
-					winnerRounds[0].matches[i].loserGoesTo = loserRounds[1].matches[loserRounds[1].amountOfMatches + (loserRounds[0].amountOfMatches * 2) - i - 1];
-					winnerRounds[1].matches[winnerRounds[1].amountOfMatches + (loserRounds[0].amountOfMatches * 2) - i - 1].loserGoesTo = loserRounds[1].matches[loserRounds[1].amountOfMatches + (loserRounds[0].amountOfMatches * 2) - i - 1];
-				}
-			}
-			for ( i = 0; i < (winnerRounds[0].amountOfMatches - winnerRounds[1].amountOfMatches); i++) {
-				winnerRounds[1].matches[i].loserGoesTo = loserRounds[1].matches[i];
-			}
-		} else {
-			for ( i = 0; i < winnerRounds[0].amountOfMatches; i++) {
-				winnerRounds[0].matches[i].loserGoesTo = loserRounds[0].matches[loserRounds[0].amountOfMatches - i - 1];
-				winnerRounds[1].matches[winnerRounds[1].amountOfMatches - i - 1].loserGoesTo = loserRounds[0].matches[loserRounds[0].amountOfMatches - i - 1];
-			}
-			for ( i = 0; i < (winnerRounds[1].amountOfMatches - winnerRounds[0].amountOfMatches); i++) {
-				winnerRounds[1].matches[i].loserGoesTo = loserRounds[1].matches[i];
+				winnerRounds[0].matches[i].winnerGoesTo = winnerRounds[1].matches[winnerRounds[0].amountOfMatches - i - 1].name;
 			}
 		}
 	}
 
 	// Create the Linked List
 	// Winner rounds - Winner goes to
-	if (tournament.byes) {
+	if (bracket.byes) {
 		i = 1;
 	} else {
 		i = 0;
 	}
 	var count = 0;
-	for (; i < (tournament.numOfWinnerRounds - 1); i++) {
+	for (; i < (bracket.numOfWinnerRounds - 1); i++) {
 		count = 0;
 		for ( j = 0; j < winnerRounds[i].amountOfMatches; j++) {
 			if (j < winnerRounds[i + 1].amountOfMatches) {
-				winnerRounds[i].matches[j].winnerGoesTo = winnerRounds[i+1].matches[j];
+				winnerRounds[i].matches[j].winnerGoesTo = winnerRounds[i+1].matches[j].name;
 			} else {
 				count++;
-				winnerRounds[i].matches[j].winnerGoesTo = winnerRounds[i+1].matches[j - count];
+				winnerRounds[i].matches[j].winnerGoesTo = winnerRounds[i+1].matches[j - count].name;
 				count++;
 			}
 		}
 	}
 
+	for ( i = 0; i < bracket.numOfWinnerRounds; i++) {
+		bracket.winnerRounds[i] = winnerRounds[i];
+	}
+}
+
+function doubleEliminationBracket(bracket) {
+	// Add the final Round to the Winner bracket
+	bracket.winnerRounds[bracket.numOfWinnerRounds] = new Object();
+	bracket.winnerRounds[bracket.numOfWinnerRounds].name = "Winners Round " + (bracket.numOfWinnerRounds + 1);
+	bracket.winnerRounds[bracket.numOfWinnerRounds].amountOfMatches = 1;
+	bracket.winnerRounds[bracket.numOfWinnerRounds].matches = new Array();
+	bracket.winnerRounds[bracket.numOfWinnerRounds].matches[0] = new Object();
+	bracket.winnerRounds[bracket.numOfWinnerRounds].matches[0].name = "Winners Round " + (bracket.numOfWinnerRounds + 1) + " Match 1";
+	bracket.numOfWinnerRounds++;
+
+	var loserRounds = new Array();
+	loserRounds[0] = new Object();
+	loserRounds[0].name = "Losers Round 1";
+	//console.log(loserRounds[0].name);
+	loserRounds[0].amountOfMatches = getLosersRound1Matches(bracket.numOfPlayers);
+	//console.log("Amount of matches: " + loserRounds[0].amountOfMatches);
+	bracket.numOfLoserRounds++;
+
+	// Calculate loser rounds matches
+	i = 0;
+	if (bracket.winnerRounds[0].amountOfMatches > bracket.winnerRounds[1].amountOfMatches) {
+		loserRounds[1] = new Object();
+		loserRounds[1].name = "Losers Round 2";
+		//console.log(loserRounds[1].name);
+		loserRounds[1].amountOfMatches = bracket.winnerRounds[1].amountOfMatches;
+		//console.log("Amount of matches: " + loserRounds[1].amountOfMatches);
+		bracket.numOfLoserRounds++;
+		i = 1;
+	}
+	var j = 1;
+	var count = 0;
+	do {
+		i++;
+		loserRounds[i] = new Object();
+		loserRounds[i].name = "Losers Round " + (i + 1);
+		//console.log(loserRounds[i].name);
+		loserRounds[i].amountOfMatches = (bracket.winnerRounds[j].amountOfMatches) / 2;
+		//console.log("Amount of matches: " + loserRounds[i].amountOfMatches);
+		bracket.numOfLoserRounds++;
+		count++;
+		if (!(count % 2)) {
+			j++;
+		}
+	} while(loserRounds[i].amountOfMatches > 1);
+	i++;
+	loserRounds[i] = new Object();
+	loserRounds[i].name = "Losers Round " + (i + 1);
+	//console.log(loserRounds[i].name);
+	loserRounds[i].amountOfMatches = 1;
+	//console.log("Amount of matches: " + loserRounds[i].amountOfMatches);
+	bracket.numOfLoserRounds++;
+
+	// Create the Matches for the Loser Rounds
+	for ( i = 0; i < bracket.numOfLoserRounds; i++) {
+		loserRounds[i].matches = new Array();
+		for ( j = 0; j < loserRounds[i].amountOfMatches; j++) {
+			loserRounds[i].matches[j] = new Object();
+			loserRounds[i].matches[j].name = "Losers Round " + (i + 1) + " Match " + (j + 1);
+			//console.log(loserRounds[i].matches[j].name);
+		}
+	}
+	// Go ahead and set the 'winner goes to' Match here, since its the last round of the losers bracket, the winner always goes to the final
+	loserRounds[i-1].matches[0].winnerGoesTo = bracket.winnerRounds[bracket.numOfWinnerRounds-1].matches[0].name;
+
+	if (bracket.byes) {
+		// Create the Linked Lists
+		// Losers Round 1 - Winner goes to
+		for ( i = 0; i < loserRounds[0].amountOfMatches; i++) {
+			if (i < (loserRounds[0].amountOfMatches - loserRounds[1].amountOfMatches)) {
+				loserRounds[0].matches[i].winnerGoesTo = loserRounds[1].matches[loserRounds[0].amountOfMatches + i - ((loserRounds[0].amountOfMatches - loserRounds[1].amountOfMatches) * 2)].name;
+			} else {
+				loserRounds[0].matches[i].winnerGoesTo = loserRounds[1].matches[loserRounds[0].amountOfMatches - i - 1].name;
+			}
+		}
+
+		// Winners Round 1 & 2 - Loser goes to
+		if (bracket.winnerRounds[0].amountOfMatches > loserRounds[0].amountOfMatches) {
+			for ( i = 0; i < bracket.winnerRounds[0].amountOfMatches; i++) {
+				if (i < (bracket.winnerRounds[0].amountOfMatches - bracket.winnerRounds[1].amountOfMatches)) {
+					bracket.winnerRounds[0].matches[i].loserGoesTo = loserRounds[0].matches[i].name;
+				} else if (i < ((bracket.winnerRounds[0].amountOfMatches - bracket.winnerRounds[1].amountOfMatches) * 2)) {// Checked
+					bracket.winnerRounds[0].matches[i].loserGoesTo = loserRounds[0].matches[loserRounds[0].amountOfMatches - (i % loserRounds[0].amountOfMatches) - 1].name;
+					// .name makes it explode. Apparantelly it going out of bounds
+				} else {
+					bracket.winnerRounds[0].matches[i].loserGoesTo = loserRounds[1].matches[loserRounds[1].amountOfMatches + (loserRounds[0].amountOfMatches * 2) - i - 1].name;
+					bracket.winnerRounds[1].matches[bracket.winnerRounds[1].amountOfMatches + (loserRounds[0].amountOfMatches * 2) - i - 1].loserGoesTo = loserRounds[1].matches[loserRounds[1].amountOfMatches + (loserRounds[0].amountOfMatches * 2) - i - 1].name;
+				}
+			}
+			for ( i = 0; i < (bracket.winnerRounds[0].amountOfMatches - bracket.winnerRounds[1].amountOfMatches); i++) {
+				bracket.winnerRounds[1].matches[i].loserGoesTo = loserRounds[1].matches[i].name;
+			}
+		} else {// Se ve bien
+			for ( i = 0; i < bracket.winnerRounds[0].amountOfMatches; i++) {
+				bracket.winnerRounds[0].matches[i].loserGoesTo = loserRounds[0].matches[loserRounds[0].amountOfMatches - i - 1].name;
+				bracket.winnerRounds[1].matches[bracket.winnerRounds[1].amountOfMatches - i - 1].loserGoesTo = loserRounds[0].matches[loserRounds[0].amountOfMatches - i - 1].name;
+			}
+			for ( i = 0; i < (bracket.winnerRounds[1].amountOfMatches - bracket.winnerRounds[0].amountOfMatches); i++) {
+				bracket.winnerRounds[1].matches[i].loserGoesTo = loserRounds[1].matches[i].name;
+			}
+		}
+	}
+
+	// Create the Linked Lists
 	// Winner rounds - Loser goes to
-	if (tournament.byes && winnerRounds[0].amountOfMatches > loserRounds[0].amountOfMatches) {
+	if (bracket.byes/* && bracket.winnerRounds[0].amountOfMatches > loserRounds[0].amountOfMatches*/) {
 		i = 2;
 	} else {
 		i = 0;
 	}
 	var temp = i;
-	for (; i < (tournament.numOfWinnerRounds - 1); i++) {
+	for (; i < (bracket.numOfWinnerRounds - 1); i++) {
 		count = 0;
-		for ( j = 0; j < winnerRounds[i].amountOfMatches; j++) {
+		for ( j = 0; j < bracket.winnerRounds[i].amountOfMatches; j++) {
 			if (!i) {
 				if (j < loserRounds[i].amountOfMatches) {
-					winnerRounds[i].matches[j].loserGoesTo = loserRounds[i].matches[j];
+					bracket.winnerRounds[i].matches[j].loserGoesTo = loserRounds[i].matches[j].name;
 				} else {
 					count++;
-					winnerRounds[i].matches[j].loserGoesTo = loserRounds[i].matches[j - count];
+					bracket.winnerRounds[i].matches[j].loserGoesTo = loserRounds[i].matches[j - count].name;
 					count++;
 				}
-			} else {
+			} else/*if (temp > bracket.numOfLoserRounds)*/
+			{
+				// Check if by putting this condition you didn't fuck shit up by fixing the stupid error
 				if (loserRounds[temp].amountOfMatches < loserRounds[temp - 1].amountOfMatches) {
 					temp++;
 				}
-				winnerRounds[i].matches[j].loserGoesTo = loserRounds[temp].matches[((j + 2) % loserRounds[temp].amountOfMatches)];
+				//console.log(((j + loserRounds[2].amountOfMatches / 2/* + 2*/) % loserRounds[temp].amountOfMatches));
+				bracket.winnerRounds[i].matches[j].loserGoesTo = loserRounds[temp].matches[Math.floor(((j + loserRounds[2].amountOfMatches / 2/* + 2*/) % loserRounds[temp].amountOfMatches))].name;
 			}
 		}
 		temp++;
 	}
 
 	// Loser rounds - Winner goes to
-	if (tournament.byes) {
+	if (bracket.byes) {
 		i = 1;
 	} else {
 		i = 0;
 	}
 	var count = 0;
-	for (; i < (tournament.numOfLoserRounds - 1); i++) {
+	for (; i < (bracket.numOfLoserRounds - 1); i++) {
 		count = 0;
 		for ( j = 0; j < loserRounds[i].amountOfMatches; j++) {
 			if (loserRounds[i].amountOfMatches > loserRounds[i + 1].amountOfMatches) {
 				if (j < loserRounds[i + 1].amountOfMatches) {
-					loserRounds[i].matches[j].winnerGoesTo = loserRounds[i+1].matches[j];
+					loserRounds[i].matches[j].winnerGoesTo = loserRounds[i+1].matches[j].name;
 				} else {
 					count++;
-					loserRounds[i].matches[j].winnerGoesTo = loserRounds[i+1].matches[j - count];
+					loserRounds[i].matches[j].winnerGoesTo = loserRounds[i+1].matches[j - count].name;
 					count++;
 				}
 			} else {
-				loserRounds[i].matches[j].winnerGoesTo = loserRounds[i+1].matches[j];
+				loserRounds[i].matches[j].winnerGoesTo = loserRounds[i+1].matches[j].name;
 			}
 		}
 	}
-	loserRounds[tournament.numOfLoserRounds-1].matches[loserRounds[tournament.numOfLoserRounds - 1].amountOfMatches - 1].winnerGoesTo = winnerRounds[tournament.numOfWinnerRounds-1].matches[winnerRounds[tournament.numOfWinnerRounds - 1].amountOfMatches - 1];
 
-	for ( i = 0; i < tournament.numOfWinnerRounds; i++) {
-		tournament.winnerRounds[i] = winnerRounds[i];
-	}
-	for ( i = 0; i < tournament.numOfLoserRounds; i++) {
-		tournament.loserRounds[i] = loserRounds[i];
+	for ( i = 0; i < bracket.numOfLoserRounds; i++) {
+		bracket.loserRounds[i] = loserRounds[i];
 	}
 }
 
