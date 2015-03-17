e304199aeea87f462ce73f3da5f5502fa5bc8d78
Rafael Pol <rafael.pol1@upr.edu>
2015-03-16 21:51:54 -0400

    Added Nested States

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -3,29 +3,41 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
 angular.module('App', ['ionic'])
 
-
-    .config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
-
-        .state('home', {
-        url: '/home',
-        templateUrl: 'views/home/home.html'
-    });
-
-    $urlRouterProvider.otherwise('/home');
-})
-
-    .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-        if(window.cordova && window.cordova.plugins.Keyboard) {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-        if(window.StatusBar) {
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
             StatusBar.styleDefault();
         }
     });
 })
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('app', {
+            url: "/app",
+            /*An abstract state can have child states but can not get activated itself. An 'abstract' state is simply a state that
+            can't be transitioned to. It is activated implicitly when one of its descendants are activated*/
+            abstract: true,
+            templateUrl: "templates/sidebar.html",
+        })
+        .state('app.home', {
+            url: "/home",
+            templateUrl: "templates/home.html"
+        })
+        .state('app.events', {
+            url: "/events",
+            templateUrl: "templates/events.html"
+        });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
+});
