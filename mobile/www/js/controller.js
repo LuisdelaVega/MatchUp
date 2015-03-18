b7c2d9a76b248cead59ab8cd7074952f16d5e025
Rafael Pol <rafael.pol1@upr.edu>
2015-03-18 10:13:39 -0400

    Event View and Profile. Profile still in progress

--- mobile/www/js/controller.js
+++ mobile/www/js/controller.js
@@ -1,21 +1,44 @@
 var myApp = angular.module('App')
 
-myApp.controller('gameController', ['$scope', function($scope) {
+myApp.controller('gameController', ['$scope', function ($scope) {
 
     $scope.singers = ['img/csgo.jpg'];
     var moreImgs = ['img/dota2.jpg', 'img/hearthstone.jpg', 'img/leagueofeppa.jpg']
     $scope.add = function add(name) {
-        if (moreImgs.length>0)
+        if (moreImgs.length > 0)
             $scope.singers.push(moreImgs.pop());
         else {
-            moreImgs.push($scope.singers.splice(0,1)[0]);
+            moreImgs.push($scope.singers.splice(0, 1)[0]);
         }
         $scope.$broadcast('scroll.infiniteScrollComplete');
     }
     $scope.remove = function remove() {
         var num = ~~(Math.random() * $scope.singers.length);
         moreImgs.push($scope.singers[num]);
-        $scope.singers.splice(num,1)[0];
+        $scope.singers.splice(num, 1)[0];
     };
 
-}]);  
+}]);
+
+myApp.controller('EventController', function ($scope, $ionicPopover) {
+    $scope.events = 'hi';
+    $ionicPopover.fromTemplateUrl('templates/events-popover.html', {
+        scope: $scope,
+    }).then(function (popover) {
+        $scope.popover = popover;
+    });
+    $scope.openPopover = function ($event) {
+        $scope.popover.show($event);
+    };
+    $scope.closePopover = function () {
+        $scope.popover.hide();
+    };
+})
+
+myApp.controller('RegularEventController', function ($scope) {
+
+})
+
+myApp.controller('PremiumEventController', function ($scope) {
+
+});
