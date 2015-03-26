42ac45fb5de2f8addeaf302d5107ea7342e0f644
luisdelavega <luisr.delavega@gmail.com>
2015-03-25 21:43:39 -0400

    Gave the events module some love. It's supposed to send all relevant details of an event. This is a blind commit because I dont have events on my local DB. If it blows up (probably) I will commit again with a fix

--- modules/events.js
+++ modules/events.js
@@ -91,14 +91,14 @@ var getEvents = function(req, res, pg, conString) {
 			console.log(queryText);
 			break;
 		}
-		
+
 		if (where) {
 			queryText += " AND event_visibility";
 		} else {
 			queryText += " WHERE event_visibility";
 			where = true;
 		}
-		
+
 		if (queryGroupBy != " GROUP BY") {
 			queryText += queryGroupBy;
 		}
@@ -127,20 +127,98 @@ var getEvent = function(req, res, pg, conString) {
 		}
 
 		// Query the database to find the account
-		var query = client.query({
-			text : "SELECT * FROM event WHERE event_name = $1 AND event_visibility",
+		var event = new Object();
+		var queryEvent = client.query({
+			text : "SELECT event_name, event_start_date, event_location, event_venue, event_banner, event_logo, event_max_capacity, event_end_date," + " event_registration_deadline, event_rules, event_description, event_deduction_fee, event_is_online, event_type" + " bool_and(event.event_name IN (SELECT event_name FROM hosts WHERE event_name = $1)) as is_hosted FROM event WHERE event_name = $1 AND event_visibility",
 			values : [req.params.event]
 		});
-		query.on("row", function(row, result) {
+		queryEvent.on("row", function(row, result) {
 			result.addRow(row);
 		});
-		query.on("end", function(result) {
+		queryEvent.on("end", function(result) {
 			if (result.rows.length > 0) {
-				res.json(result.rows[0]);
+				event.info = result.rows[0];
+				var queryMeetup = client.query({
+					text : "SELECT meetup_name, meetup_start_date, meetup_description FROM meetup WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+					values : [req.params.event, event.info.event_start_date, event.info.event_location]
+				});
+				queryMeetup.on("row", function(row, result) {
+					result.addRow(row);
+				});
+				queryMeetup.on("end", function(result) {
+					event.meetups = result.rows;
+					var queryTournament = client.query({
+						text : "SELECT tournament_name, tournament_rules, is_team_based, tournament_start_date, tournament_check_in_deadline, competitor_fee, tournament_max_capacity," + " seed_money, tournament_type, tournament_format, score_type, number_of_people_per_group, amount_of_winners_per_group, game.*" + " FROM tournament NATURAL JOIN game WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+						values : [req.params.event, event.info.event_start_date, event.info.event_location]
+					});
+					queryTournament.on("row", function(row, result) {
+						result.addRow(row);
+					});
+					queryTournament.on("end", function(result) {
+						event.tournaments = result.rows;
+						var queryNews = client.query({
+							text : "SELECT news_number, news_title, news_content, news_date_posted FROM news WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+							values : [req.params.event, event.info.event_start_date, event.info.event_location]
+						});
+						queryNews.on("row", function(row, result) {
+							result.addRow(row);
+						});
+						queryNews.on("end", function(result) {
+							event.news = result.rows;
+							var querySponsor = client.query({
+								text : "SELECT sponsor_number, sponsor_logo, sponsor_link" + " FROM sponsors NATURAL JOIN shows WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+								values : [req.params.event, event.info.event_start_date, event.info.event_location]
+							});
+							querySponsor.on("row", function(row, result) {
+								result.addRow(row);
+							});
+							querySponsor.on("end", function(result) {
+								event.sponsors = result.rows;
+								var querySpectatorFee = client.query({
+									text : "SELECT spec_fee_name, spec_fee_amount, spec_fee_description" + " FROM spectator_fee WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+									values : [req.params.event, event.info.event_start_date, event.info.event_location]
+								});
+								querySpectatorFee.on("row", function(row, result) {
+									result.addRow(row);
+								});
+								querySpectatorFee.on("end", function(result) {
+									event.spectator_fees = result.rows;
+									var queryReview = client.query({
+										text : "SELECT review_number, review_title, review_content, star_rating, review_date_created" + " FROM review WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+										values : [req.params.event, event.info.event_start_date, event.info.event_location]
+									});
+									queryReview.on("row", function(row, result) {
+										result.addRow(row);
+									});
+									queryReview.on("end", function(result) {
+										event.reviews = result.rows;
+										if (event.info.is_hosted) {
+											var queryHost = client.query({
+												text : "SELECT organization_name, organization_logo, organization_active" + " FROM organization NATURAL JOIN hosts WHERE event_name = $1 AND event_start_date = $2 AND event_location = $3",
+												values : [req.params.event, event.info.event_start_date, event.info.event_location]
+											});
+											queryHost.on("row", function(row, result) {
+												result.addRow(row);
+											});
+											queryHost.on("end", function(result) {
+												event.host = result.rows[0];
+												res.json(event);
+												client.end();
+											});
+										} else {
+											res.json(event);
+											client.end();
+										}
+									});
+								});
+							});
+						});
+					});
+				});
 			} else {
-				return res.status(404).send('Oh, no! This event does not exist');
+				client.end();
+				res.status(404).send('Oh, no! This event does not exist');
 			}
-			client.end();
 		});
 	});
 };
