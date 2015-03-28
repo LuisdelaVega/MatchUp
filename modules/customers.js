943f2bdec5b2cae869b439a994e460319da8b9fc
luisdelavega <luisr.delavega@gmail.com>
2015-03-28 15:59:04 -0400

    Changes to comply with when an account is deleted

--- modules/customers.js
+++ modules/customers.js
@@ -1,12 +1,10 @@
 //TODO Create Organization
 //TODO Create Event
-//TODO Delete Account
 //TODO Subscribe to user
 //TODO Unsubscribe to user
 
 //TODO Calculate the matches won/lost for the profile pages
 var getMyProfile = function(req, res, pg, conString) {
-
 	var username = new Object();
 	username.params = new Object();
 	username.user = new Object();
@@ -14,7 +12,7 @@ var getMyProfile = function(req, res, pg, conString) {
 	username.user.username = req.user.username;
 
 	getUserProfile(username, res, pg, conString);
-	
+
 	console.log(req.user);
 };
 
@@ -28,7 +26,7 @@ var getUserProfile = function(req, res, pg, conString) {
 		var profile = new Object();
 		// Query the database to find the user's account
 		var profileQuery = client.query({
-			text : "SELECT customer.customer_username, customer.customer_first_name, customer.customer_last_name, customer.customer_tag, customer.customer_profile_pic, customer.customer_cover_photo, customer.customer_bio, customer.customer_country, of_email.email_address, bool_and(customer_username = $1) AS my_profile FROM customer NATURAL JOIN of_email WHERE customer_username = $2 AND customer.customer_active GROUP BY customer_username, email_address",
+			text : "SELECT customer_username, customer_first_name, customer_last_name, customer_tag, customer_profile_pic, customer_cover_photo, customer_bio, customer_country, of_email.email_address, bool_and(customer_username = $1) AS my_profile FROM customer NATURAL JOIN of_email WHERE customer_username = $2 AND customer_active GROUP BY customer_username, email_address",
 			values : [req.user.username, req.params.username]
 		});
 		profileQuery.on("row", function(row, result) {
@@ -40,7 +38,7 @@ var getUserProfile = function(req, res, pg, conString) {
 
 				// Query the database to find the user's Teams
 				var teamsQuery = client.query({
-					text : "SELECT team.team_name, team.team_logo, team.team_bio, team.team_cover_photo FROM team NATURAL JOIN plays_for WHERE customer_username = $1 AND team.team_active",
+					text : "SELECT team_name, team_logo, team_bio, team_cover_photo FROM team NATURAL JOIN plays_for WHERE customer_username = $1 AND team_active",
 					values : [req.params.username]
 				});
 				teamsQuery.on("row", function(row, result) {
@@ -51,7 +49,7 @@ var getUserProfile = function(req, res, pg, conString) {
 
 					// Query the database to find the user's Organizations
 					var organizationsQuery = client.query({
-						text : "SELECT organization.organization_name, organization.organization_logo FROM organization NATURAL JOIN belongs_to WHERE customer_username = $1 AND organization.organization_active",
+						text : "SELECT organization_name, organization_logo FROM organization NATURAL JOIN belongs_to WHERE customer_username = $1 AND organization_active",
 						values : [req.params.username]
 					});
 					organizationsQuery.on("row", function(row, result) {
@@ -82,8 +80,8 @@ var getUserProfile = function(req, res, pg, conString) {
 					});
 				});
 			} else {
+				res.status(404).send('Oh, no! This user does not exist');
 				client.end();
-				return res.status(404).send('Oh, no! This user does not exist');
 			};
 		});
 	});
@@ -97,28 +95,28 @@ var editAccount = function(req, res, pg, conString) {
 
 		var queryText = "UPDATE customer SET";
 		if (req.body.firstname) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_first_name = '" + req.body.firstname + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_first_name = '" + req.body.firstname + "'";
 		}
 		if (req.body.lastname) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_last_name = '" + req.body.lastname + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_last_name = '" + req.body.lastname + "'";
 		}
 		if (req.body.tag) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_tag = '" + req.body.tag + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_tag = '" + req.body.tag + "'";
 		}
 		if (req.body.paypal) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_paypal_info = '" + req.body.paypal + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_paypal_info = '" + req.body.paypal + "'";
 		}
 		if (req.body.profilepic) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_profile_pic = '" + req.body.profilepic + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_profile_pic = '" + req.body.profilepic + "'";
 		}
 		if (req.body.cover) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_cover_photo = '" + req.body.cover + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_cover_photo = '" + req.body.cover + "'";
 		}
 		if (req.body.bio) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_bio = '" + req.body.bio + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_bio = '" + req.body.bio + "'";
 		}
 		if (req.body.country) {
-			queryText += ((queryText === "UPDATE customer SET") ? "": ",") + " customer_country = '" + req.body.country + "'";
+			queryText += ((queryText === "UPDATE customer SET") ? "" : ",") + " customer_country = '" + req.body.country + "'";
 		}
 
 		if (!req.body.firstname && !req.body.lastname && !req.body.tag && !req.body.paypal && !req.body.profilepic && !req.body.cover && !req.body.bio && !req.body.country) {
@@ -141,23 +139,35 @@ var editAccount = function(req, res, pg, conString) {
 	});
 };
 
-// Turn an Organization inactive
 var deleteAccount = function(req, res, pg, conString) {
 	pg.connect(conString, function(err, client, done) {
 		if (err) {
 			return console.error('error fetching client from pool', err);
 		}
 
-		var deleteQuery = client.query({
-			text : "UPDATE customer SET customer_active = FALSE WHERE customer_username = $1",
+		var profileQuery = client.query({
+			text : "SELECT customer_username FROM captain_for NATURAL JOIN owns WHERE customer_username = $1 AND customer_active",
 			values : [req.user.username]
-		}, function(err, result) {
-			if (err) {
-				res.status(400).send("Oh, no! Disaster!");
-				client.end();
+		});
+		profileQuery.on("row", function(row, result) {
+			result.addRow(row);
+		});
+		profileQuery.on("end", function(result) {
+			if (result.rows.length > 0) {
+				var deleteQuery = client.query({
+					text : "UPDATE customer SET customer_active = FALSE WHERE customer_username = $1",
+					values : [req.user.username]
+				}, function(err, result) {
+					if (err) {
+						res.status(500).send("Oh, no! Disaster!");
+						client.end();
+					} else {
+						res.status(204).send('');
+					}
+				});
 			} else {
-				req.user = null; //TODO This doesn't work
-				res.status(204).send('');
+				res.status(403).send("Oh, no! Relinquish your captain/owner titles before deleting dummy");
+				client.end();
 			}
 		});
 	});
@@ -221,44 +231,58 @@ var createTeam = function(req, res, pg, conString) {
 
 		console.log(req.body.info);
 		client.query("START TRANSACTION");
-		client.query({
-			text : "INSERT INTO team (team_name, team_logo, team_bio, team_cover_photo, team_active) VALUES ($1, $2, $3, $4, TRUE)",
-			values : [req.body.name, // team_name*
-			req.body.logo, // team_logo*
-			req.body.bio, // team_bio*
-			req.body.cover // team_cover_photo*
-			]
-		}, function(err, result) {
-			if (err) {
-				res.status(400).send("Oh, no! Disaster!");
-				client.end();
-			} else {
+
+		var profileQuery = client.query({
+			text : "SELECT customer_username FROM customer WHERE customer_username = $1 AND customer_active",
+			values : [req.user.username]
+		});
+		profileQuery.on("row", function(row, result) {
+			result.addRow(row);
+		});
+		profileQuery.on("end", function(result) {
+			if (result.rows.length > 0) {
 				client.query({
-					text : "INSERT INTO plays_for (customer_username, team_name) VALUES ($1, $2)",
-					values : [req.user.username, req.body.name // team_name
+					text : "INSERT INTO team (team_name, team_logo, team_bio, team_cover_photo, team_active) VALUES ($1, $2, $3, $4, TRUE)",
+					values : [req.body.name, // team_name*
+					req.body.logo, // team_logo*
+					req.body.bio, // team_bio*
+					req.body.cover // team_cover_photo*
 					]
 				}, function(err, result) {
 					if (err) {
-						res.status(400).send("Oh, no! Disaster in INSERT INTO plays_for!");
+						res.status(400).send("Oh, no! Disaster!");
 						client.end();
 					} else {
 						client.query({
-							text : "INSERT INTO captain_for (customer_username, team_name) VALUES ($1, $2)",
+							text : "INSERT INTO plays_for (customer_username, team_name) VALUES ($1, $2)",
 							values : [req.user.username, req.body.name // team_name
 							]
 						}, function(err, result) {
 							if (err) {
-								res.status(400).send("Oh, no! Disaster in INSERT INTO captain_for!");
+								res.status(400).send("Oh, no! Disaster in INSERT INTO plays_for!");
 								client.end();
 							} else {
-								res.status(201).send("HELLOOOOOO!");
-								//TODO Send something here if needed
-								client.query("COMMIT");
-								client.end();
+								client.query({
+									text : "INSERT INTO captain_for (customer_username, team_name) VALUES ($1, $2)",
+									values : [req.user.username, req.body.name]
+								}, function(err, result) {
+									if (err) {
+										res.status(400).send("Oh, no! Disaster in INSERT INTO captain_for!");
+										client.end();
+									} else {
+										res.status(201).send("HELLOOOOOO!");
+										//TODO Send something here if needed
+										client.query("COMMIT");
+										client.end();
+									}
+								});
 							}
 						});
 					}
 				});
+			} else {
+				res.status(401).send("Oh, no! It seems this user does not exist");
+				client.end();
 			}
 		});
 	});
--- modules/teams.js
+++ modules/teams.js
@@ -36,7 +36,7 @@ var getTeam = function(req, res, pg, conString) {
 				var team = new Object();
 				team.info = result.rows[0];
 				var playersQuery = client.query({
-					text : "SELECT customer.customer_username, customer.customer_first_name, customer.customer_last_name, customer.customer_tag, customer.customer_profile_pic, bool_and(customer.customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM customer NATURAL JOIN plays_for WHERE customer_active AND team_name = $1 GROUP BY customer.customer_username",
+					text : "SELECT customer_username, customer_first_name, customer_last_name, customer_tag, customer_profile_pic, bool_and(customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM customer NATURAL JOIN plays_for WHERE customer_active AND team_name = $1 GROUP BY customer_username",
 					values : [req.params.team]
 				});
 				playersQuery.on("row", function(row, result) {
@@ -49,7 +49,7 @@ var getTeam = function(req, res, pg, conString) {
 				});
 			} else {
 				client.end();
-				return res.status(404).send('Oh, no! This team does not exist');
+				res.status(404).send('Oh, no! This team does not exist');
 			};
 		});
 	});
@@ -63,21 +63,21 @@ var editTeam = function(req, res, pg, conString) {
 
 		var queryText = "UPDATE team SET";
 		if (req.body.logo) {
-			queryText += ((queryText === "UPDATE team SET") ? "": ",") + " team_logo = '" + req.body.logo + "'";
+			queryText += ((queryText === "UPDATE team SET") ? "" : ",") + " team_logo = '" + req.body.logo + "'";
 		}
 		if (req.body.bio) {
-			queryText += ((queryText === "UPDATE team SET") ? "": ",") + " team_bio = '" + req.body.bio + "'";
+			queryText += ((queryText === "UPDATE team SET") ? "" : ",") + " team_bio = '" + req.body.bio + "'";
 		}
 		if (req.body.cover) {
-			queryText += ((queryText === "UPDATE team SET") ? "": ",") + " team_cover = '" + req.body.cover + "'";
+			queryText += ((queryText === "UPDATE team SET") ? "" : ",") + " team_cover = '" + req.body.cover + "'";
 		}
 
 		if (!req.body.logo && !req.body.bio && !req.body.cover) {
 			client.end();
-			return res.status(401).send("Oh no! Disaster");
+			res.status(401).send("Oh no! Disaster");
 		}
 
-		queryText += " WHERE team_name = '" + req.params.team + "' AND team_name IN (SELECT team_name FROM plays_for WHERE customer_username = '" + req.user.username + "') AND team_active";
+		queryText += " WHERE team_name = '" + req.params.team + "' AND team_name IN (SELECT team_name FROM plays_for WHERE customer_username = '" + req.user.username + "' AND customer_active) AND team_active";
 		console.log(queryText);
 		var teamsQuery = client.query({
 			text : queryText
@@ -139,7 +139,7 @@ var addTeamMember = function(req, res, pg, conString) {
 
 		client.query("START TRANSACTION");
 		var memberQuery = client.query({
-			text : "SELECT team_name FROM plays_for NATURAL JOIN team WHERE customer_username = $1 AND team_active AND team_name = $2",
+			text : "SELECT team_name FROM plays_for NATURAL JOIN team WHERE customer_username = $1 AND team_active AND customer_active AND team_name = $2",
 			values : [req.user.username, req.params.team]
 		});
 		memberQuery.on("row", function(row, result) {
@@ -147,21 +147,35 @@ var addTeamMember = function(req, res, pg, conString) {
 		});
 		memberQuery.on("end", function(result) {
 			if (result.rows.length > 0) {
-				client.query({
-					text : "INSERT INTO plays_for (customer_username, team_name) VALUES ($1, $2)",
+				var memberQuery = client.query({
+					text : "SELECT customer_username FROM customer WHERE customer_username = $1 AND customer_active",
 					values : [req.params.username, req.params.team]
-				}, function(err, result) {
-					if (err) {
-						res.status(400).send("Oh, no! This user already plays for this team dummy");
-						client.end();
+				});
+				memberQuery.on("row", function(row, result) {
+					result.addRow(row);
+				});
+				memberQuery.on("end", function(result) {
+					if (result.rows.length > 0) {
+						client.query({
+							text : "INSERT INTO plays_for (customer_username, team_name) VALUES ($1, $2)",
+							values : [req.params.username, req.params.team]
+						}, function(err, result) {
+							if (err) {
+								res.status(400).send("Oh, no! This user already plays for this team dummy");
+								client.end();
+							} else {
+								client.query("COMMIT");
+								res.status(201).send('This user has been added! Yay!');
+							}
+						});
 					} else {
-						client.query("COMMIT");
-						res.status(201).send('This user has been added! Yay!');
+						res.status(400).send("Oh, no! This user does not exist");
+						client.end();
 					}
 				});
 			} else {
 				client.end();
-				return res.status(401).send('Oh, no! It seems you are not part of this team');
+				res.status(401).send('Oh, no! It seems you are not part of this team');
 			}
 		});
 	});
@@ -176,7 +190,7 @@ var removeTeamMember = function(req, res, pg, conString) {
 		client.query("START TRANSACTION");
 		// Look for whether or not the user that issued the request belongs to this team and if he/she captains the team
 		var queryMe = client.query({
-			text : "SELECT customer_username, bool_and(customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM plays_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active GROUP BY customer_username",
+			text : "SELECT customer_username, bool_and(customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM customer NATURAL JOIN plays_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active AND customer_active GROUP BY customer_username",
 			values : [req.params.team, req.user.username]
 		});
 		queryMe.on("row", function(row, result) {
@@ -187,7 +201,7 @@ var removeTeamMember = function(req, res, pg, conString) {
 				var reqMember = result.rows[0];
 				// Do the same for the user that is to be removed
 				var queryMember = client.query({
-					text : "SELECT customer_username, bool_and(customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM plays_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active GROUP BY customer_username",
+					text : "SELECT customer_username, bool_and(customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM customer NATURAL JOIN plays_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active AND customer_active GROUP BY customer_username",
 					values : [req.params.team, req.params.username]
 				});
 				queryMember.on("row", function(row, result) {
@@ -212,16 +226,16 @@ var removeTeamMember = function(req, res, pg, conString) {
 							});
 						} else {
 							client.end();
-							return res.status(401).send('Oh, no! It seems you are do not have enough privileges to do this');
+							res.status(401).send('Oh, no! It seems you are do not have enough privileges to do this');
 						}
 					} else {
 						client.end();
-						return res.status(401).send('Oh, no! It seems this user is not a member of this team');
+						res.status(401).send('Oh, no! It seems this user is not a member of this team');
 					}
 				});
 			} else {
 				client.end();
-				return res.status(401).send('Oh, no! It seems you are not a member of this team');
+				res.status(401).send('Oh, no! It seems you are not a member of this team');
 			}
 		});
 	});
@@ -232,10 +246,10 @@ var makeCaptain = function(req, res, pg, conString) {
 		if (err) {
 			return console.error('error fetching client from pool', err);
 		}
-		
+
 		client.query("START TRANSACTION");
 		var queryMe = client.query({
-			text : "SELECT customer_username, bool_and(customer_username IN (SELECT customer_username FROM captain_for WHERE team_name = $1)) AS is_captain FROM plays_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active GROUP BY customer_username",
+			text : "SELECT customer_username FROM customer NATURAL JOIN captain_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active AND customer_active GROUP BY customer_username",
 			values : [req.params.team, req.user.username]
 		});
 		queryMe.on("row", function(row, result) {
@@ -244,34 +258,47 @@ var makeCaptain = function(req, res, pg, conString) {
 		queryMe.on("end", function(result) {
 			if (result.rows.length > 0) {
 				var reqMember = result.rows[0];
-				if (reqMember.is_captain) {
-					client.query({
-						text : "DELETE FROM captain_for WHERE customer_username = $1 AND team_name = $2",
-						values : [reqMember.customer_username, req.params.team]
-					}, function(err, result) {
-						if (err) {
-							res.status(400).send("Oh, no! Disaster!");
-							client.end();
-						} else {
-							client.query({
-								text : "INSERT INTO captain_for (customer_username, team_name) VALUES ($1, $2)",
-								values : [req.params.username, req.params.team]
-							}, function(err, result) {
-								if (err) {
-									res.status(400).send("Oh, no! Disaster!");
-									client.end();
-								} else {
-									client.query("COMMIT");
-									client.end();
-									res.status(201).send("Yay " + req.params.username + " has beed made captain!");
-								}
-							});
-						}
-					});
-				} else {
-					client.end();
-					res.status(401).send('Oh no! It seems you are not the captain of this team');
-				}
+				var queryMember = client.query({
+					text : "SELECT customer_username FROM customer NATURAL JOIN plays_for NATURAL JOIN team WHERE team_name = $1 AND customer_username = $2 AND team_active AND customer_active GROUP BY customer_username",
+					values : [req.params.team, req.params.username]
+				});
+				queryMember.on("row", function(row, result) {
+					result.addRow(row);
+				});
+				queryMember.on("end", function(result) {
+					if (result.rows.length > 0) {
+						// var member = result.rows[0];
+						client.query({
+							text : "DELETE FROM captain_for WHERE customer_username = $1 AND team_name = $2",
+							values : [reqMember.customer_username, req.params.team]
+						}, function(err, result) {
+							if (err) {
+								res.status(500).send("Oh, no! Disaster!");
+								client.end();
+							} else {
+								client.query({
+									text : "INSERT INTO captain_for (customer_username, team_name) VALUES ($1, $2)",
+									values : [req.params.username, req.params.team]
+								}, function(err, result) {
+									if (err) {
+										res.status(500).send("Oh, no! Disaster!");
+										client.end();
+									} else {
+										client.query("COMMIT");
+										client.end();
+										res.status(201).send("Yay " + req.params.username + " has beed made captain!");
+									}
+								});
+							}
+						});
+					} else {
+						client.end();
+						res.status(401).send("Oh, no! It seems " + req.params.username +" is not a member of " + req.params.team);
+					}
+				});
+			} else {
+				client.end();
+				res.status(401).send("Oh no! It seems you ("+ req.user.username +") are not the captain of " + req.params.team);
 			}
 		});
 	});
