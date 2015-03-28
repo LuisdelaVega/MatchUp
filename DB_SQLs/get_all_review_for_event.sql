aeef46d6ac7e8b35d9b37a48f1a8596fa227de75
Edwin O. Badillo <edwin.o.badillo@ieee.org>
2015-03-28 15:15:01 -0400

    Added review queries

---
+++ DB_SQLs/get_all_review_for_event.sql
@@ -0,0 +1,2 @@
+-- gets all reviews for this event
+SELECT * FROM review WHERE event_name = 'Event 01' AND event_start_date = '2015-03-25 09:00:00' AND event_location = 'University of Puerto Rico at Mayagüez, Mayaguez, PR 00681-9042';
