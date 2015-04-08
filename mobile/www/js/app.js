7a5d9ada71b66505c5d6d7164fb3a54c9e862301
SamDLT <network.samdlt@gmail.com>
2015-04-07 21:08:23 -0400

    hot fix. tab view for popular games on iOS is now working

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -216,6 +216,10 @@ angular.module('App', ['ionic', 'wu.masonry', 'ionic.rating', 'home' , 'premium-
         .state('app.game.summary', {
         url: "/summary/:gamename",
         views: {
+            'game-summary-tab': {
+                templateUrl: "templates/gameProfile/game-summary.html",
+                controller: "gameProfileSummaryController"
+            },
             'game-upcoming-tab': {
                 templateUrl: "templates/gameProfile/game-upcoming.html",
                 controller: "gameProfileUpcomingController"
@@ -228,10 +232,6 @@ angular.module('App', ['ionic', 'wu.masonry', 'ionic.rating', 'home' , 'premium-
                 templateUrl: "templates/gameProfile/game-history.html",
                 controller: "gameProfileHistoryController"
 
-            },
-            'game-summary-tab': {
-                templateUrl: "templates/gameProfile/game-summary.html",
-                controller: "gameProfileSummaryController"
             }
         }
     })
