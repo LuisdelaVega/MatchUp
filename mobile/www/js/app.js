22307b8bedb5b7676e3e460b9e1d2979d734c633
SamDLT <network.samdlt@gmail.com>
2015-04-12 18:04:19 -0400

    si esto no funciona...

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -455,20 +455,20 @@ angular.module('App', ['ionic', 'wu.masonry', 'ionic.rating', 'home' , 'premium-
 
 })
 //Factory that interacts with the camera API of the targeted platform and allows us to use the camera.
-    .factory('Camera', ['$q', function ($q) {
-
-        return {
-            getPicture: function (options) {
-                var q = $q.defer();
-
-                navigator.camera.getPicture(function (result) {
-                    // Do any magic you need
-                    q.resolve(result);
-                }, function (err) {
-                    q.reject(err);
-                }, options);
-
-                return q.promise;
-            }
-        }
-    }]);
+//    .factory('Camera', ['$q', function ($q) {
+//
+//        return {
+//            getPicture: function (options) {
+//                var q = $q.defer();
+//
+//                navigator.camera.getPicture(function (result) {
+//                    // Do any magic you need
+//                    q.resolve(result);
+//                }, function (err) {
+//                    q.reject(err);
+//                }, options);
+//
+//                return q.promise;
+//            }
+//        }
+//    }]);
